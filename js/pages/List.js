2825ecc037251bc2d4f67ca59c2986e183174f98
bobinguise <noobripper77@gmail.com>
2025-10-25 11:15:18 +0100

    Remove video evidence requirement for hard demons
    Removed redundant information about video evidence for demon verifications.

--- js/pages/List.js
+++ js/pages/List.js
@@ -124,9 +124,6 @@ export default {
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
                     </p>
                     <p>
-                        All Demon Verifications/Completions At Or Above Hard Demon Requires Video Evidence.
-                    </p>
-                    <p>
                         Don't be a dick.
                     </p>
                 </div>
